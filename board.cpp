41ba6b2d85b376d295299e7e120f36e02b6d5697
vy007vikas <wicked.yadav@gmail.com>
2014-08-30 00:41:47 +0530

    Added initial files

---
+++ board.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Move{
+    public:
+        Move(){
+
+        }
+};
+
+class Board{
+    private:
+        int arr[N][N];
+    public:
+        int currScore;
+        Board(){
+
+        }
+};
---
+++ runner.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#include "board.cpp"
+#include "algo.cpp"
+
+using namespace std;
+
+class game{
+    public:
+        game(){
+            //initalize a game
+
+        }
+        void display(){
+            //to display the game board
+
+        }
+        void clearScreen(){
+            //to clear the screen after one move
+
+        }
+};
+
+int main(){
+
+    return 0;
+}
